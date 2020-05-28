1ad56a54daa7e84c14b5936a019d1a9b707c0954
leandro-gft <lpsacchi@gmail.com>
2020-05-28 12:19:49 -0300

    App finalizado, versão 1

--- app/src/main/java/br/com/gft/caraoucoroa/CaraOuCoroaActivity.java
+++ app/src/main/java/br/com/gft/caraoucoroa/CaraOuCoroaActivity.java
@@ -3,12 +3,26 @@ package br.com.gft.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class CaraOuCoroaActivity extends AppCompatActivity {
+
+    private ImageView imgCaraCoroa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cara_ou_coroa);
+
+        imgCaraCoroa = findViewById(R.id.imgCaraCoroa);
+        Random random = new Random();
+        imgCaraCoroa.setImageResource(random.nextInt(R.drawable.moeda_coroa-R.drawable.moeda_cara+1)+R.drawable.moeda_cara);
+    }
+
+    public void voltar(View view){
+        finish();
     }
 }
--- app/src/main/java/br/com/gft/caraoucoroa/MainActivity.java
+++ app/src/main/java/br/com/gft/caraoucoroa/MainActivity.java
@@ -2,13 +2,25 @@ package br.com.gft.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void jogar(View view){
+        Intent intent = new Intent(this, CaraOuCoroaActivity.class);
+        startActivity(intent);
     }
 }
